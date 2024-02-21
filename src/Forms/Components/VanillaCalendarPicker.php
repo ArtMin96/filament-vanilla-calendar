ec080fb9c284db0e63c0b9177a3ff79a27008533
ArtMin96 <ArtMin96@users.noreply.github.com>
2024-02-21 22:00:42 +0000

    Fix styling

--- src/Forms/Components/VanillaCalendarPicker.php
+++ src/Forms/Components/VanillaCalendarPicker.php
@@ -34,7 +34,7 @@ class VanillaCalendarPicker extends TextInput
 
     protected array $localeWeekday = [];
 
-    protected function setUp() : void
+    protected function setUp(): void
     {
         parent::setUp();
 
